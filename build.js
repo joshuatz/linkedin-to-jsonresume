a4c4a8ebfa3594ae7882a8819b4c298d40fa3279
Joshua Tzucker <joshua.tz@gmail.com>
2019-06-06 09:35:20 -0700

    looks like having the link directly in md is not going to work. Rolling back

--- build.js
+++ build.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 // Paths
 var buildFolder = './build/';
 var installFileHtml = buildFolder + 'install-page.html';
-var installFileMd = './github/' + 'install-page.md';
 var srcFolder = './src/';
 
 // Get entire contents of processed bookmarklet code as var
@@ -13,13 +12,12 @@ var bookmarkletContent = fs.readFileSync(buildFolder + 'bookmarklet_export.js');
 
 // Copy template install page to build folder
 fs.copyFileSync(srcFolder + 'install-page-template.html',installFileHtml);
-fs.copyFileSync(srcFolder + 'install-page-template.md',installFileMd);
 
 // Replace placeholder variable in install HTML file with raw contents
 replace({
     regex : "{{bookmarklet_code}}",
     replacement : bookmarkletContent,
-    paths : [installFileHtml,installFileMd],
+    paths : [installFileHtml],
     recursive : true,
     silent : false
 });
--- src/install-page-template.md
+++
@@ -1,2 +0,0 @@
-# Bookmarklet Installer:
-Drag this link - <a href="{{bookmarklet_code}}">LinkedIn-To-ResumeJSON</a> - to your bookmarks. Then click on it while viewing a LinkedIn profile page.

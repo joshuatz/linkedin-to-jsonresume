f28b74e298b137fc7cd14dedf08cb87473aa30b9
Joshua Tzucker <joshua.tz@gmail.com>
2019-08-04 12:09:36 -0700

    prep-dirs change

--- prep-dirs.js
+++ prep-dirs.js
@@ -1,5 +1,5 @@
 const fse = require('fs-extra');
-const requiredDirs = ['./build-bookmarklet', './build-browserext', './webstore-zips'];
+const requiredDirs = ['./build','./build-bookmarklet', './build-browserext', './webstore-zips'];
 
 for (let x= 0; x<requiredDirs.length; x++){
     if (!fse.existsSync(requiredDirs[x])){

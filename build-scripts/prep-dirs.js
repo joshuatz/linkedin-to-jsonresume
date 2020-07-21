6003a74cbe4fef87af963bdeac580ea1068e5e85
Joshua Tzucker <joshua.tz@gmail.com>
2020-07-21 14:12:14 -0700

    Fix: clean build dirs before archive

--- build-scripts/prep-dirs.js
+++ build-scripts/prep-dirs.js
@@ -1,9 +1,18 @@
 const fse = require('fs-extra');
 
-const requiredDirs = ['../build', '../build-bookmarklet', '../build-browserext', '../webstore-zips'];
+const requiredDirs = [`${__dirname}/../build`, `${__dirname}/../build-bookmarklet`, `${__dirname}/../build-browserext`, `${__dirname}/../webstore-zips`];
+
+// Directories to always empty first
+const cleanDirs = [`${__dirname}/../build`, `${__dirname}/../build-bookmarklet`, `${__dirname}/../build-browserext`];
 
 for (let x = 0; x < requiredDirs.length; x++) {
     if (!fse.existsSync(requiredDirs[x])) {
         fse.mkdirSync(requiredDirs[x]);
+    }
+}
+
+for (let x = 0; x < cleanDirs.length; x++) {
+    if (fse.existsSync(cleanDirs[x])) {
+        fse.emptyDirSync(cleanDirs[x]);
     }
 }
